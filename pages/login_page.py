a3bf7a186bff97d343922c7a5d878b3db57fdc78
Serge <sergekoval8@gmail.com>
2019-12-27 10:53:56 +0300

    1.)added module login_page.py for checking readress at login_page 2.) added locators for checking existing  LOGIN_FORM, REGGISTER_FORM

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Not present 'login' in current login_link"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login_form  not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.Reggister_FORM), "Reggister_form not present"
