af85f414a0b32c9d00f60d7e36aeb1df040978b8
Serge <sergekoval8@gmail.com>
2020-01-03 21:23:29 +0300

    collected tests by mark 'need_review'

--- test_product_page.py
+++ test_product_page.py
@@ -19,6 +19,7 @@ class TestUserAddToBasketFromProductPage():
         register_page.register_new_user(email, email)
         register_page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
@@ -42,6 +43,7 @@ class TestUserAddToBasketFromProductPage():
             "Success message is presented, but should not be"
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link',
                          ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -74,6 +76,8 @@ def test_guest_can_add_product_to_basket(browser, link):
     assert page.find_basket_price_product().strip() == price.strip(), "Price product in basket is different"
     time.sleep(5)
 
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -81,6 +85,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     # Гость открывает страницу товара
     # Переходит в корзину по кнопке в шапке
